fd352dcecec056682ba8390a21fad8bce879d5d1
diplmaf <lizanikolaeva432@gmail.com>
2025-12-07 21:06:08 +0300

    Update UiSelect.stories.ts

--- src/components/UiSelect/UiSelect.stories.ts
+++ src/components/UiSelect/UiSelect.stories.ts
@@ -1,1 +1,34 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import UiSelect from './UiSelect.vue'
 
+const meta: Meta<typeof UiSelect> = {
+  title: 'Components/UiSelect',
+  component: UiSelect,
+  tags: ['autodocs'],
+  argTypes: {
+    modelValue: { control: 'text' },
+    isDisabled: { control: 'boolean' },
+    options: {
+      control: 'object',
+    },
+  },
+}
+
+export default meta
+
+type Story = StoryObj<typeof UiSelect>
+
+export const Default: Story = {
+  args: {
+    modelValue: 'Опция 1',
+    options: ['Опция 1', 'Опция 2', 'Опция 3'],
+  },
+}
+
+export const Disabled: Story = {
+  args: {
+    isDisabled: true,
+    modelValue: 'Опция 1',
+    options: ['Опция 1', 'Опция 2', 'Опция 3'],
+  },
+}
