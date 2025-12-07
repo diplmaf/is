147af1bb48b605bf15fe69e3a406c93d932b9ba9
diplmaf <lizanikolaeva432@gmail.com>
2025-12-07 20:43:09 +0300

    Create UInput.vue

---
+++ src/components/UInput.vue
@@ -0,0 +1,54 @@
+<template>
+  <input
+    :class="['ui-input', { 'ui-input--disabled': isDisabled }]"
+    :value="modelValue"
+    :placeholder="placeholder"
+    :disabled="isDisabled"
+    @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+  />
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue'
+
+const props = defineProps({
+  modelValue: {
+    type: String,
+    required: true,
+  },
+  isDisabled: {
+    type: Boolean,
+    default: false,
+  },
+  placeholder: {
+    type: String,
+    default: '',
+  },
+})
+
+defineEmits(['update:modelValue'])
+</script>
+
+<style scoped lang="scss">
+@use '../styles/colors' as *;
+
+.ui-input {
+  width: 100%;
+  padding: $padding-md;
+  border: 1px solid $border-color;
+  border-radius: $border-radius;
+  font-size: $font-size-md;
+  transition: border-color 0.2s ease;
+
+  &:focus {
+    outline: none;
+    border-color: $primary;
+    box-shadow: $shadow-sm;
+  }
+
+  &--disabled {
+    background-color: $bg-light;
+    cursor: not-allowed;
+  }
+}
+</style>
