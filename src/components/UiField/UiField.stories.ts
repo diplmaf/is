73363432e2cbcea1949688f6bb8bc55082e4318b
diplmaf <lizanikolaeva432@gmail.com>
2025-12-12 09:58:19 +0300

    Update UiField.stories.ts

--- src/components/UiField/UiField.stories.ts
+++ src/components/UiField/UiField.stories.ts
@@ -1,15 +1,15 @@
 import type { Meta, StoryObj } from '@storybook/vue3'
-import { h } from 'vue'
 import UiField from './UiField.vue'
 import UiInput from '../UiInput/UiInput.vue'
+import UiSelect from '../UiSelect/UiSelect.vue'
 
 const meta: Meta<typeof UiField> = {
   title: 'Components/UiField',
   component: UiField,
   tags: ['autodocs'],
   argTypes: {
-    label: { control: 'text' },
-  },
+    label: { control: 'text' }
+  }
 }
 
 export default meta
@@ -18,16 +18,25 @@ type Story = StoryObj<typeof UiField>
 
 export const WithLabel: Story = {
   args: {
-    label: 'E-mail',
-    modelValue: '',
+    label: 'E-mail'
   },
-  render: (args) => {
-    return h(UiField, { label: args.label }, {
-      default: () => h(UiInput, {
-        modelValue: args.modelValue,
-        'onUpdate:modelValue': (value: string) => { args.modelValue = value },
-        placeholder: 'Введите email'
-      })
-    })
-  }
+  render: (args) => ({
+    components: { UiField, UiInput },
+    setup() { return { args } },
+    template: '<UiField v-bind="args"><UiInput placeholder="Введите email" /></UiField>'
+  })
+}
+
+export const WithSelect: Story = {
+  args: {
+    label: 'Выберите опцию'
+  },
+  render: (args) => ({
+    components: { UiField, UiSelect },
+    setup() {
+      const options = ['Опция 1', 'Опция 2', 'Опция 3']
+      return { args, options }
+    },
+    template: '<UiField v-bind="args"><UiSelect :options="options" /></UiField>'
+  })
 }
